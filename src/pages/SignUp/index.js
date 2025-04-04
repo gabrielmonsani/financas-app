7568e2522b01957d4027ff67bdfb825f848d3b99
gabrielmonsani <gabrieleduardomonsani@hotmail.com>
2025-04-04 16:05:25 -0300

    feat: creating sign-up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {Platform} from 'react-native';
 
-export default function SignUp(){
+import {
+  Background,
+  Container,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+} from '../SignIn/styles';
+
+export default function SignUp() {
   return (
-    <View>
-      <Text>TELA CADASTRO</Text>
-    </View>
-  )
+    <Background>
+      <Container
+      behavior={Platform.OS === 'ios' ? 'padding' : ''}
+      enabled
+      >
+
+        <AreaInput>
+        <Input
+          placeholder='Nome'
+        />
+        </AreaInput>
+
+        <AreaInput>
+        <Input
+          placeholder='Seu email'
+        />
+        </AreaInput>
+
+        <AreaInput>
+        <Input
+          placeholder='Sua senha'
+        />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+
+      </Container>
+
+    </Background>
+  );
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import { Platform } from 'react-native';
 
 const AuthStack = createNativeStackNavigator();
 
@@ -20,6 +20,16 @@ function AuthRoutes(){
       <AuthStack.Screen
         name="SignUp"
         component={SignUp}
+        options={{
+          headerStyle: {
+            backgroundColor: '#3b3dbf',
+            borderbottomWidth: 1,
+            borderBottomColor: '#00b94a',
+          },
+          headerTintColor: '#fff',
+          headerBackVisible: Platform.OS === 'ios' ? false : true,
+          title: 'Voltar',
+        }}
       />
     </AuthStack.Navigator>
   )
