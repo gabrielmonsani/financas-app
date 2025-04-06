7e33f4480b98378b4a41e26dbdb651e61e654bbf
gabriel.monsani <gabrieleduardomonsani@hotmail.com>
2025-04-05 21:43:15 -0300

    feat: user registration

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Platform} from 'react-native';
 
 import {
@@ -9,14 +9,19 @@ import {
   SubmitButton,
   SubmitText,
 } from '../SignIn/styles';
+
 import {AuthContext} from "../../contexts/auth";
 
 export default function SignUp() {
 
-  const {user} = useContext(AuthContext)
+  const {signUp} = useContext(AuthContext)
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   function handleSignUp() {
-    console.log(user)
+    signUp(email, password, nome);
   }
 
   return (
@@ -29,18 +34,25 @@ export default function SignUp() {
         <AreaInput>
         <Input
           placeholder='Nome'
+          value={nome}
+          onChangeText={ (text) => setNome(text)}
         />
         </AreaInput>
 
         <AreaInput>
         <Input
           placeholder='Seu email'
+          value={email}
+          onChangeText={ (text) => setEmail(text)}
         />
         </AreaInput>
 
         <AreaInput>
         <Input
           placeholder='Sua senha'
+          value={password}
+          onChangeText={ (text) => setPassword(text)}
+          secureTextEntry={true}
         />
         </AreaInput>
 
