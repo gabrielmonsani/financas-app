25c7d42090f4f526ceb63c3c00fa8d622d410950
gabriel.monsani <gabrieleduardomonsani@hotmail.com>
2025-04-12 18:05:53 -0300

    feat: creating new register

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Home from '../pages/Home';
+import New from '../pages/New';
 
 const AppDrawer = createDrawerNavigator();
 
@@ -23,6 +24,8 @@ function AppRoutes() {
         drawerInactiveTintColor: '#121212',
       }}>
       <AppDrawer.Screen name={'Home'} component={Home} />
+
+      <AppDrawer.Screen name={'Registrar'} component={New} />
     </AppDrawer.Navigator>
   );
 }
