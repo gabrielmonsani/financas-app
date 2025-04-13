87d9d7d146e885bd08c782a14363a247c430bd4b
gabriel.monsani <gabrieleduardomonsani@hotmail.com>
2025-04-13 03:12:06 -0300

    feat: filtering registers part 2

--- src/components/CalendarModal/index.js
+++ src/components/CalendarModal/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TouchableWithoutFeedback, View} from 'react-native';
-import {Container, ButtonFilter, ButtonFilterText, ModalContent} from './styles';
+import {
+  Container,
+  ButtonFilter,
+  ButtonFilterText,
+  ModalContent,
+} from './styles';
 
-export default function CalendarModal({setVisible}) {
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+
+export default function CalendarModal({setVisible, handleFilter}) {
+  const [dateNow, setDateNow] = useState(new Date());
+  const [markedDates, setMarkedDates] = useState({});
+
+  function handleOnDayPress(date) {
+    setDateNow(new Date(date.dateString));
+
+    let marketDay = {};
+
+    marketDay[date.dateString] = {
+      selected: true,
+      selectedColor: '#3b3dbf',
+      textColor: '#fff',
+    };
+
+    setMarkedDates(marketDay);
+  }
+
+  function handleFilterDate() {
+    handleFilter(dateNow);
+    setVisible();
+  }
+
   return (
     <Container>
       <TouchableWithoutFeedback onPress={setVisible}>
@@ -10,7 +39,18 @@ export default function CalendarModal({setVisible}) {
       </TouchableWithoutFeedback>
 
       <ModalContent>
-        <ButtonFilter>
+        <Calendar
+          onDayPress={handleOnDayPress}
+          markedDates={markedDates}
+          enableSwipeMonths={true}
+          theme={{
+            todayTextColor: '#ff0000',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#fff',
+          }}
+        />
+
+        <ButtonFilter onPress={handleFilterDate}>
           <ButtonFilterText>Filtrar</ButtonFilterText>
         </ButtonFilter>
       </ModalContent>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -67,6 +67,11 @@ export default function Home() {
     }
   }
 
+  function filterDateMovements(dateSelected) {
+    //console.log(dateSelected);
+    setDateMovements(dateSelected);
+  }
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
@@ -99,6 +104,7 @@ export default function Home() {
       <Modal visible={modalVisible} animationType={'fade'} transparent={true}>
         <CalendarModal
           setVisible={() => setModalVisible(false)}
+          handleFilter={filterDateMovements}
         />
       </Modal>
     </Background>
